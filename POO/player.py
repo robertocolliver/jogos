f40e784b46a5628110628d7ec0c2beadb1f97479
Roberto <robertocoliver@protonmail.com>
2022-11-18 15:31:46 -0300

    Jogo usando conhecimento de POO

--- POO/player.py
+++ POO/player.py
@@ -1,42 +1,48 @@
 from karos import Paladina, Vampiro
+import random 
+
+npcs = ['Rodrigo', 'Camilla', 'Renata', 'Fernanda', 'Beatriz', 'Maria']
+personagem_npcs = ['atirador', 'paladina', 'maga', 'feitiçeira', 'guerreiro', 'assassino']
 
 class Pessoas:
     
     def __init__(self, nome=None, personagens=[]):
-        if nome:
-            self.nome = nome
-        else: 
-            self.nome = 'NPC'
-        
+        self.nome = random.choice(npcs)
         self.personagens = personagens
 
+    
     def __str__(self):
         return self.nome
 
 
     def mostrar_personagens(self):
-        for c in self.personagens:
-            print(c, end='  ')
+        if personagens:
+            for c in self.personagens:
+                print(c, end='  ')
+        else:
+            print('Não tem personagens')
 
-
-class jogadores(Pessoas):
+class jogadores(Pessoas): 
     tipo = 'jogador'
-    
+   
+    lista_jogador = []
+    def escolher_personagem(self, personagem):
+        self.lista_jogador.append(personagem)
+        print(personagem)
 
-
-class vendedores(Pessoas):
-    tipo = 'vendedores'
-
-
-class inimigos(Pessoas):
+class Inimigos(Pessoas):
     tipo = 'inimigo'
+    
+    def __init__(self, nome=None, personagens=[]):
+        super().__init__(nome=nome, personagens=personagens)
+        
+        if not personagens:
+            for c in range(1, 5):
+                personagens.append(random.choice(personagem_npcs))
 
 
-paladina = Paladina(True, True, 'Carolina', level=1)
-assassino = Vampiro(True, 'Robson', level=1)
 
-
-
-jogador = jogadores(nome='Roberto', personagens=[paladina, assassino])
-print(jogador.nome)
-jogador.mostrar_personagens()
+assassino = Vampiro(adaga=True, nome='Renato', level='1')
+paladina = Paladina(espada=True, escudo=True, nome='Carolis', level=1, classe='Templaria')
+eu = jogadores(nome='Roberto', personagens=[assassino, paladina])
+eu.mostrar_personagens()
